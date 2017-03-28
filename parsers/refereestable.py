5bf89fdf6e35a7160a90539ad0aa2a41c9b5d39f
sebastian <sebastian.donosog@gmail.com>
2017-03-28 14:47:38 +0200

    converted to function qualifiers, referees and teams

--- parsers/refereestable.py
+++ parsers/refereestable.py
@@ -6,41 +6,43 @@ import time
 
 #set parameters name of tournament and season year
 
-tournament="La Liga"
-year="2015-2016"
+def referees(tournament,year):
+    print(time.strftime("%Y-%m-%d %H:%M:%S")," starting referees table")
+    savepath = "../CSV/"+tournament+"/"+year+"/"
+    pathfiles = "../JSON/"+tournament+"/"+year+"/"
+    files = os.listdir(pathfiles)
+    if not os.path.exists(savepath):
+                os.makedirs(savepath)
 
+    aux_list=[]
 
-path = "../JSON/"+tournament+"/"+year+"/"
-files = os.listdir(path)
-savepath = "../CSV/"+tournament+"/"+year+"/"
-if not os.path.exists(savepath):
-            os.makedirs(savepath)
-
-#load satisfiedevents dictionary
-e = open("../JSON/Other/events.json","r")
-eventsindex = json.load(e)
-e.close()
-eventsindex = {v: k for k, v in eventsindex.items()}
-
-aux_list=[]
-
-for file in files:
-    try:
-        f = open(str(path)+file,"r")
-        match = json.load(f)
-        matchid = f.name.split("/")[4].replace(".json","")
-        f.close()
-    except UnicodeDecodeError:
-        f = open(str(path)+file,"r",encoding="utf-8")
-        match = json.load(f,encoding="utf-8")
-        matchid = f.name.split("/")[4].replace(".json","")
-        f.close()
-        referees_dict = {"refereeid":match["referee"]["officialId"],
-                    "referee":match["referee"]["name"],
-                       }
+    for file in files:
+        try:
+            f = open(str(pathfiles)+file,"r")
+            match = json.load(f)
+            matchid = f.name.split("/")[4].replace(".json","")
+            f.close()
+        except UnicodeDecodeError:
+            f = open(str(pathfiles)+file,"r",encoding="utf-8")
+            match = json.load(f,encoding="utf-8")
+            matchid = f.name.split("/")[4].replace(".json","")
+            f.close()
+        referees_dict = {"refereeid":None,
+                        "refereename":None,
+                           }
+        try:
+            referees_dict["refereeid"] = match["referee"]["officialId"]
+        except KeyError:
+            referees_dict["refereeid"] = None
+        try:
+            referees_dict["refereename"] = match["referee"]["name"]
+        except KeyError:
+            referees_dict["refereename"] = None
         aux_list.append(referees_dict)
-    print(file," done")
-referees_df = pd.DataFrame(aux_list)
-referees_df = referees_df.drop_duplicates()
-referees_df = referees_df[["refereeid","referee"]]
-referees_df.to_csv(savepath+"referees.csv")
+    print(time.strftime("%Y-%m-%d %H:%M:%S")," season done!")
+    referees_df = pd.DataFrame(aux_list)
+    referees_df = referees_df.drop_duplicates("refereeid")
+    referees_df = referees_df[["refereeid","refereename"]]
+    referees_df.index.name = "id"
+    referees_df.to_csv(savepath+"referees.csv")
+    print(time.strftime("%Y-%m-%d %H:%M:%S")," csv file done!")
